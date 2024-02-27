c47e895f2cb50c866cbdcb06ebe0eee984f34b0d
JackKausch <72360762+JackKausch@users.noreply.github.com>
2024-02-26 19:58:17 -0500

    Added functionality to ClassyNames.py

--- HPO/ClassExplorer.py
+++ HPO/ClassExplorer.py
@@ -11,54 +11,61 @@ import pandas as pd
 
 def ClassyAnd(class1,class2,dictionary,dataframe):
     dataframe = dataframe.sort_values(by=['value'], ascending=True)
-    mean = (dictionary.get(class1)+dictionary.get(class2))/2
+    mean = abs((dictionary.get(class1)+dictionary.get(class2))/2)
     values = []
     for i in dataframe['value']:
-        values.append(i)
-    print(values)
-    for i in values:
-        i = i%mean
-    print(values)
-    dataframe['value'].transform(lambda x: x + mean)
-    dataframe = dataframe.sort_values(by=['value'], ascending=True)
-   # print(dataframe)
-    return(dataframe['name'])
+        values.append(i%mean)
+    dataframe['value'] = values
+
+    dataframe = dataframe.sort_values(by=['value'], ascending=False)
+    name= []
+    for i in dataframe['name']:
+        name.append(i)
+    return(dataframe)
+    
 
 
 def ClassyOr(class1,class2,dictionary,dataframe):
     dataframe = dataframe.sort_values(by=['value'], ascending=True)
-    print(dataframe)
     orMean = abs(dictionary.get(class1)+dictionary.get(class2)-(dictionary.get(class1)*dictionary.get(class2)))
+    values = []
     for i in dataframe['value']:
-        i = abs(i)
-        i = i%orMean
-        return(dataframe)
-    dataframe = dataframe.sort_values(by=['value'], ascending=True)
-    print(dataframe)
-    return(dataframe['name'])
+        values.append(i%orMean)
+    dataframe['value'] = values
+
+    dataframe = dataframe.sort_values(by=['value'], ascending=False)
+    name= []
+
+    for i in dataframe['name']:
+        name.append(i)
+    return(dataframe)
+    
 
 
 def ClassyNot(class1, dictionary, dataframe):
     dataframe = dataframe.sort_values(by=['value'], ascending=True)
-    print(dataframe)
     notMean = abs(1-dictionary.get(class1))
-    print(dataframe)
+    values = []
     for i in dataframe['value']:
-        i = abs(i)
-        i = i%notMean
-        return(dataframe)
-    dataframe = dataframe.sort_values(by=['value'], ascending=True)
-    print(dataframe)
-    return(dataframe['name'])
+        values.append(i%notMean)
+    dataframe['value'] = values
+       
+    dataframe = dataframe.sort_values(by=['value'], ascending=False)
+    name= []
+
+    for i in dataframe['name']:
+        name.append(i)
+    return(dataframe)
+    
 
 
-classynames = pd.read_csv('hpo.embeddings54.csv')
+classynames = pd.read_csv('human.phenotype.ontology.embeddings.csv')
 classynames_dict = classynames.set_index('name')['value'].to_dict()    
 
-#print(classynames['name'])
 
-#ClassyNot('cell',classynames_dict,classynames)
-#ClassyOr('pedal','abnormality',classynames_dict,classynames)
-ClassyAnd('pedal','abnormality',classynames_dict,classynames)
+query1 = ClassyNot('MA:0000538',classynames_dict,classynames)
+query2 = ClassyOr('chew','http://purl.obolibrary.org/obo/UBERON_0004727',classynames_dict,classynames)
+query3 = ClassyAnd('pedal','abnormality',classynames_dict,classynames)
 
-#print(classynames_dict)
+print(query1)
+
